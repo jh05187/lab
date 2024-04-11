b70797eb1cef61e4c62c591c9e1a90bd1d37175e
IHatePython375 <nguyendinhhung375@gmail.com>
2024-04-11 13:34:01 -0400

    dsadasda

--- sejklg.py
+++ sejklg.py
@@ -5,10 +5,23 @@ def menu():
     print("2. Decode")
     print("3. Quit")
 
-def encode(passcode, newpass):
-    for i in range(0, len(passcode)):
-        newpass += str(int(passcode[i]) + 3)
-        print("Your password has been encoded and stored!")
+def encode(n):
+    my_list = []
+    new_list = []
+    str_out = ''
+    for i in n:
+        my_list.append(int(i))
+
+    for num in my_list:
+        if num < 7:
+            new_list.append(num+3)
+        else:
+            new_list.append((num+3)%10)
+
+    for num in new_list:
+        str_out += str(num)
+
+    return str_out
 def decode(passcode, newpass):
     for i in range(0, len(passcode)):
         newpass += str(int(passcode[i]) - 3)
